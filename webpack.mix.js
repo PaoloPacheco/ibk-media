dd2a9064d10927dfa8bc22a4cb2e2069df88f6f1
Paolo Pacheco <paolopacheco23@gmail.com>
2023-08-18 00:15:59 -0500

    Ajustes en works 32 y 33

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,23 @@
 let mix = require('laravel-mix');
 
-mix.sass('works/work-33/styles.scss', 'works/work-33');
+mix.options({
+  postCss: [
+    require('autoprefixer')({
+      overrideBrowserslist: [
+        '> 1%',
+        'ie >= 8',
+        'edge >= 15',
+        'ie_mob >= 10',
+        'ff >= 45',
+        'chrome >= 45',
+        'safari >= 7',
+        'opera >= 23',
+        'ios >= 7',
+        'android >= 4',
+        'bb >= 10',
+      ],
+    }),
+  ],
+});
+
+mix.sass('works/work-32/styles.scss', 'works/work-32');
